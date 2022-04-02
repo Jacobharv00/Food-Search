064063c96704114d81b3ddcb74a7b61ba0d67084
jacob <jacobharv00@gmail.com>
2022-04-02 16:03:07 -0600

    setup new app; finish section 8

---
+++ src/components/SearchBar.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { View, TextInput, StyleSheet } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+
+const SearchBar = ( { term, onTermChange, onTermSubmit } ) => {
+  return (
+    <View style={ styles.backgroundStyle }>
+      <Feather name="search" style={ styles.icon } />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={ false }
+        placeholder="Search"
+        style={ styles.input }
+        value={ term }
+        onChangeText={ onTermChange }
+        onEndEditing={ onTermSubmit }
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create( {
+  backgroundStyle: {
+    backgroundColor: '#F0EEEE',
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    marginVertical: 10,
+    flexDirection: 'row',
+  },
+  icon: {
+    marginHorizontal: 15,
+    fontSize: 35,
+    alignSelf: 'center',
+  },
+  input: {
+    flex: 1,
+    fontSize: 18,
+  },
+} )
+
+export default SearchBar
